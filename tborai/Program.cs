cb9b6871d5056d800b3dd8e7645abc53f8119751
Balazs <t.balazs521@gmail.com>
2023-05-19 10:36:47 +0200

    koztes allapot

--- tborai/Program.cs
+++ tborai/Program.cs
@@ -8,26 +8,20 @@ namespace tborai
     {
         static void Main()
         {
-            List<string[]> adatok = new List<string[]>();  
+            List<string[]> adatok = new List<string[]>();
+            StreamReader sr = new StreamReader("pilotak.csv");
+            sr.Close();
+            while (!sr.EndOfStream)
+            {string[] adat = sr.ReadLine().Split(';');
+                pilota.Add(new Pilota(adat[0], adat[1], adat[2], (adat[3].Length > 0) ? int.Parse(adat[3]):0));}
+            sr.Close ();
+            Console.WriteLine($"3.feladat:{Pilotak.Count()}");
+            Console.WriteLine($"4.feladat:");
+            Console.WriteLine($"5.feladat:");
+            Console.WriteLine($"6.feladat:");
+            Console.WriteLine($"7.feladat:");
 
 
-            using (StreamReader reader = new StreamReader("pilotak.csv"))
-            {
-                string fejlec = reader.ReadLine();  
-
-
-                while (!reader.EndOfStream)
-                {
-                    string sor = reader.ReadLine();
-                    string[] adatSor = sor.Split(';'); 
-
-                    adatok.Add(adatSor);  
-                }
-            }
-
-            int adatSorokSzama = adatok.Count;  
-
-            Console.WriteLine("3. feladat: " + adatSorokSzama);
         }
     }
 }
--- tborai/pilota.cs
+++ tborai/pilota.cs
@@ -8,5 +8,21 @@ namespace tborai
 {
     internal class pilota
     {
+
+        public string name { get; set; }
+        public string szuletes { get; set; }
+        public string nemzet { get; set; }
+        public int rszam { get; set; }
+        public int year { get; set; }
+
+        public Pilota(string sor)
+        {
+            var adat = sor.Split(';');
+            name = adat[0];
+            szuletes = adat[1];
+            nemzet = adat[2];
+            rszam = (adat[3].Length > 0) ? int.Parse(adat[3]) : 0;
+            year = int.Parse(adat[1].Substring(0, 4));
+        }
     }
 }
