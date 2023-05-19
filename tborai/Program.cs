b0eed9a97ff718c0e9bd685587a659d093d5477f
Balazs <t.balazs521@gmail.com>
2023-05-19 10:00:42 +0200

    szunet_elotti_mentes

--- tborai/Program.cs
+++ tborai/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace tborai
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string[]> adatok = new List<string[]>();  
+
+
+            using (StreamReader reader = new StreamReader("pilotak.csv"))
+            {
+                string fejlec = reader.ReadLine();  
+
+
+                while (!reader.EndOfStream)
+                {
+                    string sor = reader.ReadLine();
+                    string[] adatSor = sor.Split(';'); 
+
+                    adatok.Add(adatSor);  
+                }
+            }
+
+            int adatSorokSzama = adatok.Count;  
+
+            Console.WriteLine("3. feladat: " + adatSorokSzama);
+        }
+    }
+}
